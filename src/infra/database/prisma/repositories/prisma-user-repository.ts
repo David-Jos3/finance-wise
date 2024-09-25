b226b709353bf81cc932a20f9d73a1a5bb285c3b
David-jos3 <davidgomesgc230@gmail.com>
2024-09-24 22:31:51 -0300

    chore: add contracts and implementations for user repositories

---
+++ src/infra/database/prisma/repositories/prisma-user-repository.ts
@@ -0,0 +1,29 @@
+import { User } from '@/domain/entities/user'
+import { UserRepository } from '@/domain/repositories/user-repository'
+import { PrismaUserMapper } from '../mapper/prisma-user-mapper'
+import { PrismaService } from '../../prisma.service'
+import { Injectable } from '@nestjs/common'
+
+@Injectable()
+export class PrismaUserRepository implements UserRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async create(user: User): Promise<void> {
+    const data = PrismaUserMapper.toPrisma(user)
+
+    await this.prisma.user.create({
+      data,
+    })
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    })
+    if (!user) {
+      return null
+    }
+
+    return PrismaUserMapper.toDomain(user)
+  }
+}

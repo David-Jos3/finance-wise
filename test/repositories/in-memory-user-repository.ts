035524dbdf5d4d73a9423d39b370e31b680ade40
David-jos3 <davidgomesgc230@gmail.com>
2024-09-24 22:36:05 -0300

    chore: add in-memory user repository for testing

---
+++ test/repositories/in-memory-user-repository.ts
@@ -0,0 +1,14 @@
+import { User } from '@/domain/entities/user'
+import { UserRepository } from '@/domain/repositories/user-repository'
+
+export class InMemoryUserRepository implements UserRepository {
+  public items: User[] = []
+
+  async create(user: User): Promise<void> {
+    this.items.push(user)
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.items.find((item) => item.email === email)
+  }
+}

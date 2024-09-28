7fd0e505a5e40a860ccd591d1193f4d0a268140c
David-jos3 <davidgomesgc230@gmail.com>
2024-09-27 21:19:04 -0300

    feat: implement JWT strategy and global auth guard using RS256

--- src/infra/http/auth/auth.module.ts
+++ src/infra/http/auth/auth.module.ts
@@ -3,6 +3,10 @@ import { PassportModule } from '@nestjs/passport'
 import { JwtModule } from '@nestjs/jwt'
 import { ConfigService } from '@nestjs/config'
 import { Env } from '@/infra/env/env'
+import { JwtStrategy } from './jwt.strategy'
+import { EnvService } from '@/infra/env/env.service'
+import { JwtAuthGuard } from './jwt-auth.guard'
+import { APP_GUARD } from '@nestjs/core'
 
 @Module({
   imports: [PassportModule, JwtModule.registerAsync({
@@ -11,13 +15,21 @@ import { Env } from '@/infra/env/env'
       const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true })
       const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true })
       return {
-        privateKey,
-        publicKey,
-        signOptions: { expiresIn: '1h' },
+        signOptions: { expiresIn: '1h', algorithm: 'RS256' },
+        privateKey: Buffer.from(privateKey, 'base64'),
+        publicKey: Buffer.from(publicKey, 'base64'),
       }
     },
 
   }),
+  ],
+  providers: [
+    JwtStrategy,
+    EnvService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
   ],
   exports: [JwtModule],
 })

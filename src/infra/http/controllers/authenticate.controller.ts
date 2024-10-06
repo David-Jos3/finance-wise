1c61c77aca3fd5a609ecf860d0d585201eeac761
David-jos3 <davidgomesgc230@gmail.com>
2024-10-05 23:11:38 -0300

    feat: add controller for authentication and implement tests

---
+++ src/infra/http/controllers/authenticate.controller.ts
@@ -0,0 +1,52 @@
+/* eslint-disable @stylistic/max-len */
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  UnauthorizedException,
+  UsePipes,
+} from '@nestjs/common'
+import { z } from 'zod'
+import { ZodValidationPipe } from '../pipe/zod-validation-pipe'
+import { AuthenticateUserUseCase } from '@/domain/use-cases/authenticate-user'
+import { Public } from '../auth/public'
+import { right } from '@/core/either'
+import { WrongCredentialsError } from '@/domain/use-cases/errors/wrong-credentials-error'
+
+const authenticateUserBodySchema = z.object({
+  email: z.string().email(),
+  password: z.string()
+    .min(8, { message: 'Password must be at least 8 characters long' }),
+})
+
+type AuthenticateBodySchema = z.infer<typeof authenticateUserBodySchema>
+
+@Controller('/sessions')
+@UsePipes(new ZodValidationPipe(authenticateUserBodySchema))
+export class AuthenticateController {
+  constructor(private authenticateUserUseCase: AuthenticateUserUseCase) {}
+
+  @Public()
+  @Post()
+  @HttpCode(201)
+  async handle(@Body() body: AuthenticateBodySchema) {
+    const { email, password } = body
+
+    const result = await this.authenticateUserUseCase.execute({
+      email,
+      password,
+    })
+
+    if (result.isLeft()) {
+      const error = result.value
+      if (error instanceof WrongCredentialsError) {
+        throw new UnauthorizedException()
+      }
+      throw new BadRequestException()
+    }
+
+    return right({ access_token: result.value.accessToken })
+  }
+}

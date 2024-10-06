54b94425ff57badff225674f34414390dcd73422
David-jos3 <davidgomesgc230@gmail.com>
2024-10-05 23:02:09 -0300

    feat: restructure AppModule with Config, Http, Auth, and Env modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './infra/env/env'
+import { EnvService } from './infra/env/env.service'
+import { HttpModule } from './infra/http/http.module'
+import { AuthModule } from './infra/http/auth/auth.module'
+import { EnvModule } from './infra/env/env.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    HttpModule,
+    AuthModule,
+    EnvModule,
+  ],
+  controllers: [],
+  providers: [EnvService],
 })
 export class AppModule {}

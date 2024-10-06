05588b89f2f250099a50091298a156fea808d92a
David-jos3 <davidgomesgc230@gmail.com>
2024-10-05 23:49:17 -0300

    feat: add error handling and validation to CreateAccountController

--- src/infra/http/controllers/create-account.controller.ts
+++ src/infra/http/controllers/create-account.controller.ts
@@ -1,7 +1,10 @@
+/* eslint-disable @stylistic/max-len */
 import { RegisterUserUseCase } from '@/domain/use-cases/register-user'
-import { Body, Controller, HttpCode, Post, UsePipes } from '@nestjs/common'
+import { BadRequestException, Body, ConflictException, Controller, HttpCode, Post, UsePipes } from '@nestjs/common'
 import { z } from 'zod'
 import { ZodValidationPipe } from '../pipe/zod-validation-pipe'
+import { Public } from '../auth/public'
+import { UserAlreadyExistsError } from '@/domain/use-cases/errors/user-already-exists-error'
 
 const createAccountBodySchema = z.object({
   name: z.string(),
@@ -16,16 +19,23 @@ type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
 @UsePipes(new ZodValidationPipe(createAccountBodySchema))
 export class CreateAccountController {
   constructor(private registerUserUseCase: RegisterUserUseCase) {}
+  @Public()
   @Post()
   @HttpCode(201)
   async handle(@Body() body: CreateAccountBodySchema) {
     const { name, email, password } = body
-    console.log('Controller')
-    console.log({ name, email, password })
-    await this.registerUserUseCase.execute({
+
+    const result = await this.registerUserUseCase.execute({
       name,
       email,
       password,
     })
+    if (result.isLeft()) {
+      const error = result.value
+      if (error instanceof UserAlreadyExistsError) {
+        throw new ConflictException(error.message)
+      }
+      throw new BadRequestException(error)
+    }
   }
 }

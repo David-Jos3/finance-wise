ce486e2df3f8cef2af426459e6dcbae4709a2e19
David-jos3 <davidgomesgc230@gmail.com>
2024-10-05 23:01:33 -0300

    chore: add path alias support in Vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,4 @@
+import path from 'node:path'
 import swc from 'unplugin-swc'
 import { defineConfig } from 'vitest/config'
 
@@ -5,6 +6,11 @@ export default defineConfig({
   test: {
     globals: true,
     root: './',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   plugins: [
     swc.vite({

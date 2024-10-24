3b79a9cf6568b6bc8bcb6556d0b460a1db143327
David-jos3 <davidgomesgc230@gmail.com>
2024-10-23 21:52:41 -0300

    feat: add controller for deleting a category

---
+++ src/infra/http/controllers/delete-category.controller.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @stylistic/max-len */
+import { DeleteCategoryUseCase } from '@/domain/use-cases/delete-category'
+import { BadRequestException, Controller, Delete, HttpCode, Param } from '@nestjs/common'
+import { Public } from '../auth/public'
+
+@Controller('categories/:id')
+export class DeleteCategoryController {
+  constructor(private deleteCategoryUseCase: DeleteCategoryUseCase) {}
+
+  @Delete()
+  @Public()
+  @HttpCode(204)
+  async handle(@Param('id') categoryId: string) {
+    const result = await this.deleteCategoryUseCase.execute({ categoryId })
+
+    if (result.isLeft()) {
+      return new BadRequestException()
+    }
+  }
+}

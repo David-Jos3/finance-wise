a088c998ce359391c015dfa5cd5d34ff374b38bc
David-jos3 <davidgomesgc230@gmail.com>
2024-10-29 21:11:49 -0300

    feat(core): add findById, update, and delete methods to BudgetRepository

--- src/domain/repositories/budget-repository.ts
+++ src/domain/repositories/budget-repository.ts
@@ -2,4 +2,7 @@ import { Budget } from '../entities/budget'
 
 export abstract class BudgetRepository {
   abstract create(data:Budget): Promise<void>
+  abstract findById(budgetId: string): Promise<Budget | null>
+  abstract update(budget: Budget): Promise<void>
+  abstract delete(budgetId: string): Promise<void>
 }

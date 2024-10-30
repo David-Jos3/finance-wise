0c4599131c8ef694b459f0c5a9a4dbb9c2b8a1fc
David-jos3 <davidgomesgc230@gmail.com>
2024-10-29 21:14:07 -0300

    feat(core): add findById, update, and delete methods to IncomeRepository

--- src/domain/repositories/income-repository.ts
+++ src/domain/repositories/income-repository.ts
@@ -2,4 +2,7 @@ import { Income } from '../entities/income'
 
 export abstract class IncomeRepository {
   abstract create(data: Income): Promise<void>
+  abstract findById(incomeId: string): Promise<Income | null>
+  abstract update(income: Income): Promise<void>
+  abstract delete(incomeId: string): Promise<void>
 }

4cd4a4ca912a1a4383e9d3d9cb21cb2ad264c60d
David-jos3 <davidgomesgc230@gmail.com>
2024-11-01 09:52:08 -0300

    refactor: add methods findById, update, and delete to InMemoryExpenseRepository

--- test/repositories/in-memory-expense-repository.ts
+++ test/repositories/in-memory-expense-repository.ts
@@ -7,4 +7,24 @@ export class InMemoryExpenseRepository implements ExpenseRepository {
   async create(expense: Expense): Promise<void> {
     this.items.push(expense)
   }
+
+  async findById(expenseId: string): Promise<Expense | null> {
+    return this.items.find(item => item.id === expenseId)
+  }
+
+  async update(expense: Expense): Promise<void> {
+    const index = this.items.findIndex((item) => item.id === expense.id)
+
+    if (index !== -1) {
+      this.items[index] = expense
+    }
+  }
+
+  async delete(expenseId: string): Promise<void> {
+    const index = this.items.findIndex((item) => item.id === expenseId)
+
+    if (index !== -1) {
+      this.items.splice(index, 1)
+    }
+  }
 }

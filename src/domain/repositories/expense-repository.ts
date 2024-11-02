47acd81884238cf78359f8512253023b0a77d18c
David-jos3 <davidgomesgc230@gmail.com>
2024-11-02 20:20:50 -0300

    feat(core): add findById, update, and delete methods to ExpenseRepository

--- src/domain/repositories/expense-repository.ts
+++ src/domain/repositories/expense-repository.ts
@@ -2,4 +2,7 @@ import { Expense } from '../entities/expense'
 
 export abstract class ExpenseRepository {
   abstract create(data:Expense): Promise<void>
+  abstract findById(expenseId: string): Promise<Expense | null>
+  abstract update(expense: Expense): Promise<void>
+  abstract delete(expenseId: string): Promise<void>
 }
